a552dd434acc9f3c0376a59964205cd71f1b354f
Gleb Nichyparovich <gleb.nichyparovich@gmail.com>
2024-10-24 15:50:30 +0200

    add - switched to PostgreSQL database

--- src/SelfFinanceApp.Persistance/DependencyInjection.cs
+++ src/SelfFinanceApp.Persistance/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+
 using SelfFinanceApp.Domain.Common;
 using SelfFinanceApp.Domain.Contracts;
 using SelfFinanceApp.Domain.Contracts.Repositories;
@@ -8,25 +9,31 @@ using SelfFinanceApp.Persistance.DatabaseContext;
 using SelfFinanceApp.Persistance.Health;
 using SelfFinanceApp.Persistance.Repositories;
 
-namespace SelfFinanceApp.Persistance
+namespace SelfFinanceApp.Persistance;
+
+public static class DependencyInjection
 {
-    public static class DependencyInjection
+    public static void AddPersistanceServices(this IServiceCollection services, IConfiguration configuration)
     {
-        public static void AddPersistanceServices(this IServiceCollection services, IConfiguration configuration)
+        services.AddDbContext<IDatabaseMigrator, ApplicationDbContext>(options =>
         {
-            services.AddDbContext<IDatabaseMigrator, ApplicationDbContext>(options =>
-            {
-                options.UseSqlServer(configuration.GetConnectionString("MyDbContext"));
-            });
+            options.UseNpgsql(configuration.GetConnectionString("MyDbContext"),
+                npgsqlOptionsAction: sqlOptions =>
+                {
+                    sqlOptions.EnableRetryOnFailure(
+                        maxRetryCount: 5,
+                        maxRetryDelay: TimeSpan.FromSeconds(30),
+                        errorCodesToAdd: null);
+                });
+        });
 
-            //Add Repositories
-            services.AddScoped<IFinancialTypesRepository, FinancialTypesRepository>();
-            services.AddScoped<IFinancialOperationsRepository, FinancialOperationsRepository>();
-
-            services.AddScoped<IDateTimeProvider, SystemDateTimeProvider>();
-
-            services.AddHealthChecks()
-                .AddCheck<DatabaseHealthCheck>(DatabaseHealthCheck.Name);
-        }
+        //Add Repositories
+        services.AddScoped<IFinancialTypesRepository, FinancialTypesRepository>();
+        services.AddScoped<IFinancialOperationsRepository, FinancialOperationsRepository>();
+        services.AddScoped<IDateTimeProvider, SystemDateTimeProvider>();
+        services.AddHealthChecks()
+            .AddNpgSql(configuration.GetConnectionString("MyDbContext")!,
+                name: DatabaseHealthCheck.Name,
+                tags: new[] { "ready" });
     }
 }
