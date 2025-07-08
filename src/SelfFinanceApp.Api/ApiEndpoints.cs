b35aa6b37067a52e7077a9632b298ed3e7784737
Gleb Nichyparovich <gleb.nichyparovich@seyfor.si>
2025-07-08 11:31:05 +0200

    fix - rename endpoint naming

--- src/SelfFinanceApp.Api/ApiEndpoints.cs
+++ src/SelfFinanceApp.Api/ApiEndpoints.cs
@@ -6,32 +6,32 @@
 
         public static class FinancialTypes
         {
-            public const string Base = $"{ApiBase}/financialTypes";
+            public const string Base = $"{ApiBase}/financial-types";
 
             public const string Create = $"{Base}";
-            public const string GetAll = $"{Base}/getAll";
+            public const string GetAll = $"{Base}/get-all";
             public const string Get = $"{Base}/{{id:guid}}";
-            public const string Update =  $"{Base}/{{id:Guid}}";
+            public const string Update = $"{Base}/{{id:Guid}}";
             public const string Delete = $"{Base}/{{id:Guid}}";
             public const string Patch = $"{Base}/{{id:Guid}}";
         }
 
         public static class FinancialOperations
         {
-            public const string Base = $"{ApiBase}/financialOperations";
+            public const string Base = $"{ApiBase}/financial-operations";
 
             public const string Create = $"{Base}/create";
-            public const string GetMany = $"{Base}/getMany";
+            public const string GetMany = $"{Base}/get-many";
             public const string Get = $"{Base}/{{id:guid}}";
-            public const string Update =  $"{Base}/{{id:Guid}}";
+            public const string Update = $"{Base}/{{id:Guid}}";
             public const string Delete = $"{Base}/{{id:Guid}}";
         }
 
         public static class Reports
         {
             public const string Base = $"{ApiBase}/reports";
-            
-            public const string GetMany = $"{Base}/get";
+
+            public const string GetMany = $"{Base}/get-many";
         }
     }
 }
